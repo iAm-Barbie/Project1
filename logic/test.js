efe979874d262c04d4a41ed08417df96541d7921
Phil Mauracher <gradientus@gmail.com>
2019-08-03 13:50:33 -0600

    DUMB

--- logic/test.js
+++ logic/test.js
@@ -1,59 +1,47 @@
-$(document).ready(function() {
+$("button").on("click", function() {
   //api key, tasty chanel ID, URL
   var key = "AIzaSyABeRywiHw1NWbm9A0nb8ZG6IfG2p9HFmI";
-  var playlistId = "UCJFp8uSYCjXOMnkUyb3CQ3Q";
-  var queryURL = "https://www.googleapis.com/youtube/v3/search";
-  var userChoice = document.getElementById("results1, results2, results3");
-  var recipeSearch = userChoice.value;
-
+  var q = "dogs";
+  //var playlistId = "UCJFp8uSYCjXOMnkUyb3CQ3Q";
+  var queryURL = `https://www.googleapis.com/youtube/v3/search?key=${key}&part=snippet&q=${q}`;
+  //var recipeSearch = userChoice.value;
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    var options = {
-      part: "snippet",
-      q: recipeSearch,
-      key: key,
-      maxResults: 3,
-      playlistId: playlistId
-    };
-
-    function loadVids() {
-      $.getJSON(URL, options, function(data) {
-        var id = data.items[0].snippet.resourceId.videoId;
-        mainVid(id);
-        resultsLoop(data);
-      });
-    }
-    loadVids();
-
-    function mainVid(id) {
-      $("#video").html(`
-                      <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                  `);
-    }
-
-    function resultsLoop(data) {
-      $.each(data.items, function(i, item) {
-        var thumb = item.snippet.thumbnails.medium.url;
-        var title = item.snippet.title;
-        var desc = item.snippet.description.substring(0, 100);
-        var vid = item.snippet.resourceId.videoId;
-        $("main").append(`
-                              <article class="item" data-key="${vid}">
-<img src="${thumb}" alt="" class="thumb">
-                                  <div class="details">
-                                      <h4>${title}</h4>
-                                      <p>${desc}</p>
-                                  </div>
-                              </article>
-                          `);
-      });
-    }
-
-    $("main").on("click", "article", function() {
-      var id = $(this).attr("data-key");
+    $.getJSON(queryURL, function(response) {
+      var id = response.items[0].id.videoId;
       mainVid(id);
     });
   });
+
+  function mainVid(id) {
+    $("#video").html(`
+                      <iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                  `);
+  }
 });
+
+//     function resultsLoop(data) {
+//       $.each(data.items, function(i, item) {
+//         var thumb = item.snippet.thumbnails.medium.url;
+//         var title = item.snippet.title;
+//         var desc = item.snippet.description.substring(0, 100);
+//         var vid = item.snippet.resourceId.videoId;
+//         $("main").append(`
+//                               <article class="item" data-key="${vid}">
+//  <img src="${thumb}" alt="" class="thumb">
+//                                   <div class="details">
+//                                       <h4>${title}</h4>
+//                                       <p>${desc}</p>
+//                                   </div>
+//                               </article>
+//                           `);
+//       });
+//     }
+//     $("main").on("click", "article", function() {
+//       var id = $(this).attr("data-key");
+//       mainVid(id);
+//     });
+//   });
+//  });
