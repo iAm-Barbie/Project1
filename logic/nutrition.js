834ec2206c4790a3bef6351f1ae7ab9090bf0f6d
Phil Mauracher <gradientus@gmail.com>
2019-08-06 13:09:28 -0600

    Added the on click event listener to listen for clicks on specific foods.  Delaring an endpoint with the correct response.  Tested using philRecipes.html

--- logic/nutrition.js
+++ logic/nutrition.js
@@ -1,31 +1,30 @@
-$("button").on("click", function() {
-  //var ndbno = $("#txt_name").val();
-  var foodIdArray = [];
+$(".food").on("click", function() {
+  var ndbno = $(this).attr("data-id");
 
   //this is the endpoint
   var queryURL = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${ndbno}&type=b&format=json&api_key=dxYdoXKO6VV76LlruaXcuN0V4grH1rzCoUx8KNR1`;
 
-  //AJAX call to get the JSON we need
+  // //AJAX call to get the JSON we need
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(queryURL);
     console.log(response);
-    var foodName = response.foods[0].food.desc.name; //NOTE: might want to replace
-    var calories = response.foods[0].food.nutrients[1].unit; //NOTE: might want to replace
-    var calVal = response.foods[0].food.nutrients[1].value;
-    var nutrients = response.foods[0].food.nutrients[2].name; //NOTE: might want to replace
-    var nutVal = response.foods[0].food.nutrients[2].value;
-    var fat = response.foods[0].food.nutrients[3].name; //NOTE: might want to replace
-    var fatVal = response.foods[0].food.nutrients[3].value;
-    var carbs = response.foods[0].food.nutrients[4].name; //NOTE: might want to replace
-    var carbsVal = response.foods[0].food.nutrients[4].value;
-    $(".food").text(foodName);
-    $(".calories").text(calories + " " + calVal);
-    $(".protein").text(nutrients + " " + nutVal);
-    $(".fat").text(fat + " " + fatVal);
-    $(".carbs").text(carbs + " " + carbsVal);
+    // var foodName = response.foods[0].food.desc.name; //NOTE: might want to replace
+    // var calories = response.foods[0].food.nutrients[1].unit; //NOTE: might want to replace
+    // var calVal = response.foods[0].food.nutrients[1].value;
+    // var nutrients = response.foods[0].food.nutrients[2].name; //NOTE: might want to replace
+    // var nutVal = response.foods[0].food.nutrients[2].value;
+    // var fat = response.foods[0].food.nutrients[3].name; //NOTE: might want to replace
+    // var fatVal = response.foods[0].food.nutrients[3].value;
+    // var carbs = response.foods[0].food.nutrients[4].name; //NOTE: might want to replace
+    // var carbsVal = response.foods[0].food.nutrients[4].value;
+    // $(".food").text(foodName);
+    // $(".calories").text(calories + " " + calVal);
+    // $(".protein").text(nutrients + " " + nutVal);
+    // $(".fat").text(fat + " " + fatVal);
+    // $(".carbs").text(carbs + " " + carbsVal);
   });
 });
 
