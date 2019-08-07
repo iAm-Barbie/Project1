3b4c7f0beee42900af2e9283ccfec4c57efdf4da
Phil Mauracher <gradientus@gmail.com>
2019-08-06 20:30:50 -0600

    Removed unneeded comments.  Trying to for a push on this file too.

--- logic/nutrition.js
+++ logic/nutrition.js
@@ -3,10 +3,12 @@ $(".food").on("click", function() {
   var ndbno = $(this).attr("data-id");
   var foodType = $(this).attr("id");
 
+  //
   //this is the endpoint
   var queryURL = `https://api.nal.usda.gov/ndb/V2/reports?ndbno=${ndbno}&type=b&format=json&api_key=dxYdoXKO6VV76LlruaXcuN0V4grH1rzCoUx8KNR1`;
 
-  // //AJAX call to get the JSON we need
+  //
+  //AJAX call to get the JSON we need
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -17,6 +19,7 @@ $(".food").on("click", function() {
     var fatVal = response.foods[0].food.nutrients[3].value;
     var carbsVal = response.foods[0].food.nutrients[4].value;
 
+    //
     //appends the values to the usdaInfo table
     $("#usdaInfo").append(
       `<tr class="usdaFood"><td rowspan="2">${foodType}</td><tr>`
@@ -41,6 +44,3 @@ $(".food").on("click", function() {
 $("#clear-button").on("click", function() {
   $("#usdaInfo").empty();
 });
-
-//TODO: need to push the data attributes to the array
-//TODO: need to loop 3 times and call the API 3 times
